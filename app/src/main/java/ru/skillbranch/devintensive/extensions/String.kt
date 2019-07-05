4a6d9a0e730280d0148d6d766d6483794e9fb742
Waryozh <waryozh@yandex.ru>
2019-07-05 17:31:44 +0300

    Add String.stripHtml extension

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -7,3 +7,22 @@ fun String.truncate(size: Int = 16): String {
     }
     return res
 }
+
+fun String.stripHtmlTags(): String {
+    return this.replace("<[^>]*>".toRegex(), "")
+}
+
+fun String.stripHtmlEscapeSequences(): String {
+    return this.replace("&amp;|&lt;|&gt;|&#39;|&quot;".toRegex(), "")
+}
+
+fun String.stripInternalWhitespaces(): String {
+    return this.replace(" {2,}".toRegex(), " ")
+}
+
+fun String.stripHtml(): String {
+    return this
+        .stripHtmlTags()
+        .stripHtmlEscapeSequences()
+        .stripInternalWhitespaces()
+}
