ae1987d9270b0a2adc55ab743ad86958e5b386a2
Waryozh <waryozh@yandex.ru>
2019-07-14 15:10:38 +0300

    Add extension Activity.hideKeyboard

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -0,0 +1,12 @@
+package ru.skillbranch.devintensive.extensions
+
+import android.app.Activity
+import android.view.inputmethod.InputMethodManager
+
+fun Activity.hideKeyboard() {
+    this.currentFocus?.let { view ->
+        val imm = this.getSystemService(Activity.INPUT_METHOD_SERVICE) as? InputMethodManager
+        imm?.hideSoftInputFromWindow(view.windowToken, 0)
+        view.clearFocus()
+    }
+}
